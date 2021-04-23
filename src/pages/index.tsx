350fcfa3b0409e1f53d8deb991f5d78531133cc5
Amedir <ademirmsfilho@fac.pe.senac.br>
2021-04-22 23:18:55 -0300

    Adiciona context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,10 @@ import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 import styles from './home.module.scss';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 
-type Episodes = {
+type Episode = {
   id: string;
   title: string;
   thumbnail: string;
@@ -20,11 +22,13 @@ type Episodes = {
 }
 
 type HomeProps = {
-  latestEpisodes: Episodes[];
-  allEpisodes: Episodes[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 }
 
 export default function Home( {latestEpisodes, allEpisodes}: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -51,7 +55,7 @@ export default function Home( {latestEpisodes, allEpisodes}: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode) }>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
